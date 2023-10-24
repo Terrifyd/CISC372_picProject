dcd85060a7051ee2d4f3f54b6285239ff253955b
Dean Turner <turnerde@udel.edu>
2023-10-24 01:17:08 -0400

    added some notes, need to work on getting pthreads working

--- image.c
+++ image.c
@@ -4,6 +4,7 @@
 #include <string.h>
 #include "image.h"
 
+
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
 
@@ -58,7 +59,8 @@ uint8_t getPixelValue(Image* srcImage,int x,int y,int bit,Matrix algorithm){
 //Returns: Nothing
 void convolute(Image* srcImage,Image* destImage,Matrix algorithm){
     int row,pix,bit,span;
-    span=srcImage->bpp*srcImage->bpp;
+    span=srcImage->bpp*srcImage->bpp; // what is span used for here?
+    // PARALLIZE HERE BY ROW, may need to create another function to create threads for? or call convelute in each thread?
     for (row=0;row<srcImage->height;row++){
         for (pix=0;pix<srcImage->width;pix++){
             for (bit=0;bit<srcImage->bpp;bit++){
@@ -94,7 +96,7 @@ int main(int argc,char** argv){
     t1=time(NULL);
 
     stbi_set_flip_vertically_on_load(0); 
-    if (argc!=3) return Usage();
+    if (argc!=3) return Usage(); // why does argc have to be 3 when there are 2 argumants? Doesnt seem to affect running
     char* fileName=argv[1];
     if (!strcmp(argv[1],"pic4.jpg")&&!strcmp(argv[2],"gauss")){
         printf("You have applied a gaussian filter to Gauss which has caused a tear in the time-space continum.\n");
@@ -110,7 +112,7 @@ int main(int argc,char** argv){
     destImage.bpp=srcImage.bpp;
     destImage.height=srcImage.height;
     destImage.width=srcImage.width;
-    destImage.data=malloc(sizeof(uint8_t)*destImage.width*destImage.bpp*destImage.height);
+    destImage.data=malloc(sizeof(uint8_t)*destImage.width*destImage.bpp*destImage.height); // why use malloc?
     convolute(&srcImage,&destImage,algorithms[type]);
     stbi_write_png("output.png",destImage.width,destImage.height,destImage.bpp,destImage.data,destImage.bpp*destImage.width);
     stbi_image_free(srcImage.data);
